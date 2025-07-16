16a81b8dc2abf9d3ede467467cfdaeb4e07f466c
2279679717@qq.com <2279679717@qq.com>
2025-07-16 17:38:29 +0800

    feat(api):bug修复

---
+++ src/CyDrag/component/react-beautiful-dnd.d.ts
@@ -0,0 +1,7 @@
+declare module 'react-beautiful-dnd' {
+  export type DraggableId = string;
+  export type DraggableLocation = {
+    droppableId: string;
+    index: number;
+  };
+}
--- src/CyDrag/component/types.ts
+++ src/CyDrag/component/types.ts
@@ -1,15 +1,20 @@
-// @flow
-import type { DraggableId, DraggableLocation } from "react-beautiful-dnd/types";
+// TypeScript类型定义
+// @ts-ignore
 
 export type Id = string;
 
-export type Quote = {|
-  id: Id,
-  content: string,
-  author: Author,
-|};
-
-export type QuoteMap = {
-  [key: string]: Quote[],
+export type Author = {
+  id: string;
+  name: string;
+  avatar: string;
 };
 
+export type Quote = {
+  id: Id;
+  content: string;
+  author: Author;
+};
+
+export type QuoteMap = {
+  [key: string]: Quote[];
+};
