5b9639520fbc4b78ebe9b808513cdafd4869fef9
2279679717@qq.com <2279679717@qq.com>
2025-07-16 17:22:57 +0800

    feat(api):bug修复

--- docs/examples/mobile-dialog/basic.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from 'antd-mobile';
-import { Dialog } from '../../../src/mobile';
-
-export default () => (
-  <Dialog closeOnMaskClick trigger={<Button color="warning">Open Dialog</Button>}>
-    I ❤️ Ant Design Mobile
-  </Dialog>
-);
